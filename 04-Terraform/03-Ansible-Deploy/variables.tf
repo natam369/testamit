4ac01882dc590161ce651b758d20053806526286
root <root@mtadminnuvepro.fxkhnfp2o1hejkwmlm3rqlql3e.cx.internal.cloudapp.net>
2020-02-02 11:41:02 +0000

    yoyo

--- 04-Terraform/03-Ansible-Deploy/variables.tf
+++ 04-Terraform/03-Ansible-Deploy/variables.tf
@@ -1,9 +1,9 @@
 variable "key_name" {
-  default = "devops-my-aws-keypair"
+  default = "awsarnab"
 }
 
 variable "pvt_key" {
-  default = "/root/.ssh/amit-personal-aws.pem"
+  default = "/root/.ssh/mydevops-aws.pem"
 }
 
 variable "us-east-zones" {
@@ -11,5 +11,5 @@ variable "us-east-zones" {
 }
 
 variable "sg-id" {
-  default = "sg-00c3c981ea2a2ad80"
+  default = "sg-094cfff8e82b22b0e"
 }
